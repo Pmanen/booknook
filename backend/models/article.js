e9d23e62cf66d7d647d0eb9cfac1bcf119a06d27
Pippa van Manen <pippavanmanen@icloud.com>
2026-02-16 17:55:34 +0100

    remove tracking links from urls

--- backend/models/article.js
+++ backend/models/article.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const articleSchema = mongoose.Schema({
   title: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   author: {
     type: String,
-    trim: true
+    trim: true,
   },
   outlet: {
     type: String,
-    trim: true
+    trim: true,
   },
   url: {
     type: String,
@@ -21,28 +21,29 @@ const articleSchema = mongoose.Schema({
     lowercase: true,
     trim: true,
     validate: {
-      validator: (v) => {
+      validator: v => {
         try {
-          new URL(v)
-          return true
+          new URL(v);
+          return true;
         } catch {
-          return false
+          return false;
         }
       },
-      message: props => `${props.value} is not a valid url`
+      message: props => `${props.value} is not a valid url`,
     },
-    set: (v) => {
-      v = v.trim().toLowerCase()
+    set: v => {
+      v = v.trim().toLowerCase();
       if (!/^https?:\/\//i.test(v)) {
         return `https://${v}`;
       }
-      v = v.replace(/\/$/, '')
+      v = v.replace(/\?.*$/, '');
+      v = v.replace(/\/$/, '');
       return v;
-    }
+    },
   },
   dateAdded: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   length: {
     type: Number, // read time in minutes
@@ -51,11 +52,11 @@ const articleSchema = mongoose.Schema({
     default: 0,
     validate: {
       validator: Number.isInteger,
-      message: props => `${props.value} is not an integer`
-    }
+      message: props => `${props.value} is not an integer`,
+    },
   },
   datePublished: {
-    type: Date
+    type: Date,
   },
   genreTag: {
     type: Number,
@@ -63,17 +64,18 @@ const articleSchema = mongoose.Schema({
     max: 99,
     validate: {
       validator: Number.isInteger,
-      message: props => `${props.value} is not a valid genre tag between 0 and 99`
-    }
+      message: props =>
+        `${props.value} is not a valid genre tag between 0 and 99`,
+    },
   },
-})
+});
 
 articleSchema.set('toJSON', {
   transform: (_document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Article', articleSchema)
+module.exports = mongoose.model('Article', articleSchema);
